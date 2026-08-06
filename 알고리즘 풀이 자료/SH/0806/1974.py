0406dc0f807e2b4ab9c8426f3db8987a83979115
나선환 <worlyst@gmail.com>
2026-08-06 17:56:16 +0900

    20260806 commit

--- 알고리즘 풀이 자료/SH/0806/1974.py
+++ 알고리즘 풀이 자료/SH/0806/1974.py
@@ -11,18 +11,35 @@ if 문을 사용해 겹치는 숫자가 없을 때 1을 출력, 그렇지 않을
 """
 
 # 10개의 스도쿠 게임 만듬
-T = int(input())                                      # 테스트 케이스를 몇 번 반복할 건지 개수가 입력됨 문제의 경우 10번 반복
-for test_case in range(1, T + 1):                     # 10번 반복이기 때문에 range를 사용해 1부터 10까지 반복
+T = int(input())                                      # 테스트 케이스를 몇 번 반복할 건지 개수가 입력됨 문제의 경우 10번 반복*
+for test_case in range(1, T + 1):                     # 10번 반복이기 때문에 range를 사용해 1부터 10까지 반복*
     puzzle = []                                       # 9x9의 리스트를 만들 빈 리스트 생성
     for i in range(9):                                # 리스트에 0부터 8까지 총 9개의 인덱스 범위를 설정
-        raw_data = list(map(int, input().split()))    # raw_data를 리스트 형식으로 불러오는 코드
+        raw_data = list(map(int, input().split()))    # raw_data를 리스트 형식으로 불러오는 코드*
         puzzle.append(raw_data)                       # 불러온 raw_data를 빈리스트인 puzzle에 추가
 
 
-    for nums in puzzle:
-        row = []
-        for num in nums:
+# 가로열 검증
+    for row in puzzle:                                # 퍼즐에 있는 스도쿠 한 게임을 가로열을 기준으로 하나씩 호출
+        row_nums = []                                 # 가로열에 있는 숫자를 하나씩 넣을 빈 리스트 생성
+        for num in row:                               # 가로열에 있는 숫자 9개를 하나씩 호출
             row.append(num)
+            if num in row_nums:                       # if문을 활용해 row_nums에 num이 있다면 False 출력
+                is_valid = False
+
+
+# 세로열 검증
+    for i in range(9):                                # 인덱스를 활용할 0부터 8까지 9개의 숫자를 하나씩 호출
+        col_nums = []                                 # 세로열에 있는 숫자를 모을 빈 리스트 생성
+        for column in puzzle[i]:                      # 스도쿠 한 게임을 세로열을 기준으로 숫자들을 하나씩 호출
+            col_nums.append(column)                   # 호출한 숫자들을 col_nums에 추가
+            if column in col_nums:                    # if 문을 활용해 col_nums에 column 숫자들이 있다면 False 출력
+                is_valid = False
+
+
+# 3x3 범위 검증
+
+
 
 
 
